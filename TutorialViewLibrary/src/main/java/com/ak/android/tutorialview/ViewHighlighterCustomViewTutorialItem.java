704938b43ae86937db2e7f468b01e79b313025d5
Avinash Kolluru <avinash.kolluru@gmail.com>
2014-08-22 14:16:03 +0530

    Removed dependency of BackgroundViewAnchorHighlighter in tutorial with custom layout

--- TutorialViewLibrary/src/main/java/com/ak/android/tutorialview/ViewHighlighterCustomViewTutorialItem.java
+++ TutorialViewLibrary/src/main/java/com/ak/android/tutorialview/ViewHighlighterCustomViewTutorialItem.java
@@ -24,7 +24,8 @@ public class ViewHighlighterCustomViewTutorialItem implements TutorialItem{
     }
 
     public View getView(){
-        RelativeLayout v = (RelativeLayout)LayoutInflater.from(activity).inflate(this.layoutId, null);
+        RelativeLayout parent = (RelativeLayout)LayoutInflater.from(activity).inflate(R.layout.tutorial_view_custom_layout_parent, null);
+        RelativeLayout v = (RelativeLayout)LayoutInflater.from(activity).inflate(this.layoutId, parent, true);
 
         if(-1 != viewId) {
             View anchorView = activity.findViewById(viewId);
@@ -32,7 +33,7 @@ public class ViewHighlighterCustomViewTutorialItem implements TutorialItem{
                 int[] screenLocation = new int[2];
                 anchorView.getLocationOnScreen(screenLocation);
                 final RectF viewAnchor = new RectF(screenLocation[0], screenLocation[1], screenLocation[0] + anchorView.getWidth(), screenLocation[1] + anchorView.getHeight());
-                BackgroundViewAnchorHighlighter backgroundViewHighlighter = (BackgroundViewAnchorHighlighter) v.findViewById(R.id.tutorial_frame_background_view_highlighter);
+                BackgroundViewAnchorHighlighter backgroundViewHighlighter = (BackgroundViewAnchorHighlighter) parent.findViewById(R.id.tutorial_frame_background_view_highlighter);
                 backgroundViewHighlighter.setVisibility(View.VISIBLE);
                 backgroundViewHighlighter.addViewAnchorRect(new ArrayList<RectF>() {
                                                                 {
@@ -43,6 +44,6 @@ public class ViewHighlighterCustomViewTutorialItem implements TutorialItem{
             }
         }
 
-        return v;
+        return parent;
     }
 }
